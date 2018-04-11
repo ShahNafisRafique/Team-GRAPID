044b10800982299b454987c1fb0b4a4e5573d0ba
ShahNafisRafique <38015120+ShahNafisRafique@users.noreply.github.com>
2018-04-11 10:42:25 -0400

    square object made

---
+++ objects/Square.js
@@ -0,0 +1,14 @@
+
+function Square(name,position,vertices,colors,vertexIndices)
+{
+	
+	this.name=name;
+	this.position=position;
+	this.vertices=vertices;
+	this.colors=colors;
+	this.vertexIndices=vertexIndices;
+	
+}
+
+
+
