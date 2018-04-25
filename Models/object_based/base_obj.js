f888a39c91fc7998952f1ae4a36c57f472481123
Namenai <36117049+bfp4f360@users.noreply.github.com>
2018-04-25 17:09:40 -0400

    commented base object and square pyramid object

--- Models/object_based/base_obj.js
+++ Models/object_based/base_obj.js
@@ -1,6 +1,6 @@
 class baseObj {
 	
-
+	//Sets basic attributes of the objects that are passed in.
 	constructor(_name,_position,_vertices,_color,_drawType) {
 
 		this.name=_name;
@@ -16,6 +16,7 @@ class baseObj {
 		
 	}
 	
+	//This creates the buffers based of if the buffers are not created.
 	createBuffers() {
 		if(this.positionBuffer ===undefined &&  this.vertices !==undefined)
 		{
@@ -33,57 +34,69 @@ class baseObj {
 		}
 	}
 	
-	//getters
+	//Returns color.
 	getColor() {
 		return this.color;
 	}
 	
+	//Returns the color buffer.
 	getColorBuffer() {
 		return this.colorBuffer;
 	}
 	
+	//Returns how many colors are in colro buffer.
 	getColorBufferNumItems() {
 		return this.colorBufferNumItems;
 	}
 	
+	//Returns the draw type.
 	getDrawType() {
 		return this.drawType;
 	}
 	
+	//Returns if color has been set.
 	getHasColor()
 	{
 		return this.hasColor;
 	}
 	
+	//returns if there is a 'texture'.
 	getHasTexture() {
 		return this.hasTexture;
 	}
 	
+	//Returns the index buffer.
 	getIndexBuffer() {
 		
 		return undefined;
 	}
 	
+	//returns the number of index buffer items.
 	getIndexBufferNumItems() {
 		return undefined;
 	}
 	
+	//Returns the name of the object.
 	getName(){
 		return this.name;
 	}
 	
+	//Returns the position of the object.
 	getPosition() {																									
 		return this.position;
 	}
 	
+	//Returns the positon buffer.
 	getPositionBuffer() {
 		return this.positionBuffer;
 	}
 	
+	//Returns how many position buffer items there are.
 	getPositionBufferNumItems() {
 		return this.positionBufferNumItems;
 	}
 	
+	//Returns the rotation axis. If none defaualts to x axis
 	getRotationAxis() {
 		if(this.rotationAxis===undefined)
 		{
@@ -94,6 +107,7 @@ class baseObj {
 		return this.rotationAxis;
 	}
 	
+	//Gets the rotation degree,if none defaults to 0.
 	getRotationDegree() {
 		if( this.rotationDegree===undefined)
 		{
@@ -105,6 +119,7 @@ class baseObj {
 		return this.rotationDegree;
 	}
 	
+	//Gets rotation speed, if none defaults to 0.
 	getRotationSpeed() {
 		if(this.rotationSpeed===undefined)
 		{
@@ -115,38 +130,46 @@ class baseObj {
 		return this.rotationSpeed;
 	}
 	
+	//Returns the texture coord buffer.
 	getTextureCoordBuffer() {
 		return this.textureCoordBuffer;
 	}
 	
+	//Returns the texture coordinates.
 	getTextureCoord() {
 		return this.textureCoord
 	}
 	
+	//Returns size of texture coordinate for each item.
 	getTextureCoordItemSize() {
 		return 2;
 	}
 	
+	//Returns the number of texture coord items.
 	getTextureCoordNumItem() {
 		return this.textureCoordNumItem;
 	}
 	
+	//Returns the texture index which tells webgl what texture to get from the global texture array.
 	getTextureIndex() {
 		return this.textureIndex;
 	}
 	
+	//Returns the vertices.
 	getVertices() {
 		return this.vertices;
 	}
 	
+	//Returns the vertex indices,by default is undefined, if a object has vertex indices then it overides it like in the cube class.
 	getVertexIndice() {
 		return undefined;
 	}
 	
+	//This checks the parameter and sets the color, if the passed in color is invalid defaults position and color a black square pyramid.
 	setColor(_color) {
 		
 		
-		//if vertices are undefined then default to a square baseless pyramid,
+		//If vertices are undefined then default to a square baseless pyramid.
 		if(this.vertices===undefined){
 			
 			
@@ -175,17 +198,18 @@ class baseObj {
 			];
 		}
 		
-		//if any of these are false,default to white 
+		//If any of these are false,default to white color.
 		if((this.vertices!==undefined)&& ((_color.length/4)==(this.vertices.length/3)) && (isCorrectArrayType(_color,"number")) ) {
 			console.log("color set");
 			this.color=_color;
 		}
 		else {
-			console.log("defaulting colour to black");
+			console.log("defaulting color to black");
 							
 							
 			var unpackedColors = [];
-		
+			
+			//Creates the white color set.
 			for (var j=0; j < (this.vertices.length/3); j++) {
 				unpackedColors = unpackedColors.concat([0.0,0.0,0.0,0.0]);
 			}
@@ -197,6 +221,7 @@ class baseObj {
 		
 	}//end set color	
 
+	//Checks and sets the draw type,if invalid defaults to points
 	setDrawType(_drawType){
 		if((_drawType == gl.POINTS) || (_drawType == gl.LINES) || (_drawType == gl.LINE_STRIP) || (_drawType == gl.LINE_LOOP) || (_drawType ==gl.TRIANGLES) || (_drawType ==gl.TRIANGLE_STRIP) || (_drawType ==gl.TRIANGLE_FAN))
 		{
@@ -208,11 +233,13 @@ class baseObj {
 		}
 	}
 	
+	//Sets the name.
 	setName(_name) {
 		
 		this.name=_name;
 	}
 	
+	//Sets the position but first checks it, if invalid then sets to eye positon.
 	setPosition(_pos) {
 	
 		if((this._pos!==undefined)&& ((_pos.length)==3) && (isCorrectArrayType(_pos,"number")) ) {
@@ -224,6 +251,7 @@ class baseObj {
 		}
 	}
 	
+	//Checks and sets the rotation axis passed in, if invalid sets to [0,0,0]
 	setRotationAxis(_rotAxi) {
 		
 		if(_rotAxi.length==3)
@@ -236,6 +264,7 @@ class baseObj {
 		}
 	}
 	
+	//Checks and sets the rotation degree,if invalid sets to 0 degrees.
 	setRotationDegree(_rotDeg) {
 		
 		
@@ -250,6 +279,7 @@ class baseObj {
 		}
 	}
 	
+	//Checks and sets the rotation speed, if invalid sets to 0 speed as in no speed.
 	setRotationSpeed(_speed) {
 		
 		if(typeof _speed=="number")
@@ -262,6 +292,8 @@ class baseObj {
 		}
 	}
 	
+	//Sets the texture and creates the buffers for it. Also determines if the object 'should display' the texutre.
+	//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ maybe add a checker for this?
 	setTexture(_textIndex,_textCoord,_shouldTexture) {
 		//this.textureCoordBuffer=_textBuffer;
 		
@@ -290,10 +322,12 @@ class baseObj {
 		}
 	}
 
+	//Checks and sets vertices, if invalid sets to the square pyramid vertices.
 	setVertices(_vert){
 		
 		if(_vert===undefined)
 		{
+			//Sets to square pyramid vertices
 			this.vertices=[
 			// Front face
 			 0.0,  1.0,  0.0,
@@ -314,7 +348,7 @@ class baseObj {
 			 0.0,  1.0,  0.0,
 			-1.0, -1.0, -1.0,
 			-1.0, -1.0,  1.0
-		];
+			];
 		}
 		else {
 			this.vertices=_vert;
--- Models/object_based/square_pyramid.js
+++ Models/object_based/square_pyramid.js
@@ -3,9 +3,10 @@ class SquarePyramid extends baseObj {
 	//The square Pyramid object is very basic, as of right now it appears to be the most basic 3D object so far.
 	constructor(_name,_position,_vertices,_color,_drawType) {
 
-	
+		//Calls parent constructor and passes the parameters in
 		super(_name,_position,_vertices,_color,_drawType);
 		
+		//This creates the buffer objects.
 		this.createBuffers();
 		
 	}
