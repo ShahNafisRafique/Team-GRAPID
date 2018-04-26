fd9bfea7b72e1e065013d1fc93bc278da7ce2b0b
Matthew T Smitherman <MTSMITHE@campus.uncg.edu>
2018-04-26 17:15:54 -0400

    defaults incrementally improved

--- util/default_values.js
+++ util/default_values.js
@@ -1,26 +1,60 @@
-function defaultRotationAxis() {
-	return [1,0,0];
-}
+/* 
+This file creates a closure that stores functions
+which return default values for scene-object initialization
 
-function defaultRotationDegree() {
-	return 0;
-}
+Authors: Matt Smitherman, Shah Nafis Rafique, Yoonah Lee
 
-function defaultRotationSpeed() {
-	return 0;
-}
+Last Updated: 4/26/2018
+ */
 
-function defaultColor() {
-	return [0.0,0.0,0.0,0.0];
-}
+var defaults = (function () {
+  
+  function defaultRotationAxis() {
+    return [1,0,0];
+  }
 
-function defaultColor(_vert) {
-	var unpackedColors = [];
-			
-	//Creates the white color set.
-	for (var j=0; j < _vert; j++) {
-		unpackedColors = unpackedColors.concat([0.0,0.0,0.0,0.0]);
-	}
-	
-	return unpackedColors;
-}
+  function defaultRotationDegree() {
+    return 0;
+  }
+
+  function defaultRotationSpeed() {
+    return 0;
+  }
+
+  function defaultColor() {
+    return [0.0,0.0,0.0,0.0];
+  }
+
+  function defaultColor(_vert) {
+    var unpackedColors = [];
+        
+    //Creates the white color set.
+    for (var j=0; j < _vert; j++) {
+      unpackedColors = unpackedColors.concat([0.0,0.0,0.0,0.0]);
+    }
+    
+    return unpackedColors;
+  }
+  
+  
+  return {
+   "defaultRotationAxis" : function() {
+  }
+
+   "defaultRotationDegree" : function() {
+      return 0;
+  }
+
+   "defaultRotationSpeed" : function() {
+    return 0;
+  }
+
+  function defaultColor() {
+    return [0.0,0.0,0.0,0.0];
+  }
+
+  function defaultColor(_vert) {
+    
+  };
+  
+}());
